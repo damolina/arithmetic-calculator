0dca67743372974cd0c95d0ee0461b27e83acd50
Daniel Molina <danielangelmolina@gmail.com>
2023-02-23 23:59:47 -0300

    added CORS config

--- src/main/java/com/dmolina/arithmeticcalculator/controller/UserController.java
+++ src/main/java/com/dmolina/arithmeticcalculator/controller/UserController.java
@@ -12,9 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "https://calculator-frontend.herokuapp.com", maxAge = 3600)
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
